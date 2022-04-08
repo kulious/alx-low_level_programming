237c6d709e23dd33aebf22042f490ef27def6c34
kulious <uche_onyendi@yahoo.com>
2022-04-07 22:03:26 -0700

    use putss

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * REturn: always o successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
