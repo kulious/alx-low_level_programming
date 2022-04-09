e5dbdda2484a6b2d1614aefa7e5e3994bd42c554
kulious <uche_onyendi@yahoo.com>
2022-04-08 19:52:57 -0700

    still on while loop

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,27 +2,21 @@
 
 /**
  *main - prints the alphabet in lowercase,
-
  * followed by a new line, except q and e
  * Return: Aways 0 (Sucess)
  */
 
 int main(void)
-
 {
 	char ch = 'a';
 
 	while (ch <= 'z')
 
 	{
-
 		if (ch != 'e' && ch != 'q')
-
 		{
-
-																	putchar(ch);
-																    }
-
+			putchar(ch); 				
+		}
 		ch++;
 
 	}
