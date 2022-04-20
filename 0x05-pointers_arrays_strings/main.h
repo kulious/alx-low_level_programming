1ab26a6a60055281d61cf3f0600ef058660ab681
kulious <uche_onyendi@yahoo.com>
2022-04-20 15:54:30 -0700

    maim

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _putchar(char c);
+int _putchar(char c);
 void reset_to_98(int *n); 
 void swap_int(int *a, int *b); 
 int _strlen(char *s); 
@@ -12,5 +12,4 @@ void puts2(char *str);
 void puts_half(char *str); 
 void print_array(int *a, int n); 
 char *_strcpy(char *dest, char *src);
-
-#endif
+int _atoi(char *s);
