1726e5893092013c6c6c4e625dc7ec6f992ed548
kulious <uche_onyendi@yahoo.com>
2022-05-11 19:37:49 -0700

    new changes in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,18 +9,14 @@
  * Description: The attributes of a dog.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for dog structure
- */
-
-typedef struct dog dog_t;
+dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
